fd8aeb3e1f125e1514196f01f1443d77b57d5b7e
sophia <scastellarin95@gmail.com>
2017-03-24 20:09:25 -0400

    Calculate the norm of alleles to determine genetic variability

--- wolf.py
+++ wolf.py
@@ -1,10 +1,16 @@
-# A Wolf should have 5 alleles
-
 from allele import Allele
+from math import pow
 
 class Wolf(object):
     def __init__(self, alleles):
+        # A wolf should have 5 alleles [1]
         if len(alleles) > 5:
             raise ValueError("Too many alleles specified")
         self.alleles = alleles
+
+    def genetic_variability(self):
+        return pow(sum(
+            pow(self.alleles[i].allele,2) for i in 
+            range(0, len(self.alleles))), 0.5
+        )
     
