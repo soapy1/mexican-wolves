e458cac5fe0367889f539de5745ad7cc0cf34db1
sophia <scastellarin95@gmail.com>
2017-03-25 22:37:08 -0400

    Keep track of data per year

--- run_sim.py
+++ run_sim.py
@@ -8,7 +8,13 @@ def main():
     max_population = 150
     # Pack sizes for gray wolves range from about 2 to 8 members [4]
     packs = [generate_pack(ceil(normalvariate(5,3))) for i in range(0,3)]
-
+    time_data = [{
+        'year':0,
+        'stats':{
+            'num_packs':len(packs),
+            'wolf_pop':wolf_population(packs),
+            'avg_genetic_var':wolf_pop_genetic_variance(packs)}
+        }]
     for i in range(0,15):
         year += 1;
         next_iter_packs = []
@@ -22,12 +28,21 @@ def main():
             elif len(p.wolves) > 0:
                 next_iter_packs.append(p)
         packs = next_iter_packs
+        time_data.append({
+            'year':year,
+            'stats':{
+                'num_packs':len(packs),
+                'wolf_pop':wolf_population(packs),
+                'avg_genetic_var':wolf_pop_genetic_variance(packs)}
+        })
 
 #    print("pack sizes: ", [len(p.wolves) for p in packs])
 #    print("genetic variances: ", [p.average_genetic_variance() for p in packs])
+    print(time_data)
     print("number packs: ", len(packs))
     print("wolf population: ", wolf_population(packs))
-    print("average variance: ", sum(p.average_genetic_variance() for p in packs)/len(packs))
+    print("average variance: ", wolf_pop_genetic_variance(packs))
+
 
 def split_pack(p):
     len_new_pack = ceil(len(p.wolves)/2)
@@ -39,6 +54,10 @@ def split_pack(p):
 
 def wolf_population(packs):
     return sum(len(p.wolves) for p in packs)
+
+
+def wolf_pop_genetic_variance(packs):
+    return sum(p.average_genetic_variance() for p in packs)/len(packs)
 
 
 def ages_lsp(p):
